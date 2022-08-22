49ef2a3a51dd26debb47cd023ddbbcbf576fd701
PLAxiaoxin <15820485835@163.com>
2022-08-22 18:01:50 +0800

    添加测试

--- code/src/js/index.js
+++ code/src/js/index.js
@@ -1,8 +1,6 @@
-import { invoices } from '../data/invoices.js';
-import { plays } from '../data/plays.js';
 import { createStatementData } from './createStatementData.js';
-console.log(statement(invoices, plays));
-function statement(invoice, plays) {
+
+export function textStatement(invoice, plays) {
   return renderPlanText(createStatementData(invoice, plays));
 }
 
@@ -20,24 +18,25 @@ function renderPlanText(data) {
   return result;
 }
 
-function htmlStatement(invoices, plays) {
-  return renderHtml(createStatementData(invoice, plays));
+export function htmlStatement(invoices, plays) {
+  return renderHtml(createStatementData(invoices, plays));
 }
 
 function renderHtml(data) {
   let result = `<h1>Statement for ${data.customer}</h1>`;
   result += '<Table>\n';
   result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>';
-  for (let pref of data.preformance) {
+  for (let pref of data.performances) {
     result += `<tr><td>${pref.play.name}</td><td>${pref.audience}</td>`;
     result += `<td>${usd(pref.amount)}</td></tr>/n`;
   }
   result += '</table>/n';
   result += `<p>Amount owend is <em>${usd(data.totolAmount)}</em></p>/n`;
   result += `<p>You earned <em>${usd(data.totolVolumeCredits)}</em></p>/n`;
+  return result;
 }
 
-function usd(aNumber) {
+export function usd(aNumber) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
---
+++ code/src/main.js
@@ -0,0 +1,4 @@
+import { invoices } from './data/invoices.js';
+import { plays } from './data/plays.js';
+import { textStatement, htmlStatement } from './js/index';
+console.log(textStatement(invoices, plays));
